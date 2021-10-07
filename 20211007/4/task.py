6a968d590ed35ac2a8a23c08a57ed734f1bc2111
Оконишников Арий practicum <s02190690@stud.cs.msu.ru>
2021-10-07 16:16:57 +0300

    Fourth #2

--- 20211007/4/task.py
+++ 20211007/4/task.py
@@ -3,9 +3,11 @@ def calc(s, t ,u):
 	a = lambda x: eval(s)
 	b = lambda x: eval(t)
 	c = lambda x, y: eval(u)
-	return lambda x,y: c(a, b)
+	def fun(x):
+		return(c(a(x), b(x)))
+	return fun
 	
 	
 	
 F = calc("sin(x)**2", "cos(x)**2", "x+y")
-print(123)
+print(F(123))
