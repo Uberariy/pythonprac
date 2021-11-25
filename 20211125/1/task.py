9baee4928ec69fd2f825682d7c1e11493f8c4148
Uberariy <70648019+Uberariy@users.noreply.github.com>
2021-11-26 01:20:20 +0300

    Task1 tests done

--- 20211125/1/task.py
+++ 20211125/1/task.py
@@ -1,15 +1,20 @@
-#TODO tests
+
 import sys
 l = sys.stdin.buffer.read()
 N = l[0]
 o = l[0:1]
 l = l[1:]
-s = sorted([l[i:i+len(l)//N+1] for i in range(N)])
-sys.stdout.buffer.write(s[0])
+seg = len(l)/N 
+if seg > len(l)//N:
+	seg = int(seg//1) + 1
+else:
+	seg = int(seg)
+#print(seg*N, len(l), N, sorted([l[i:i+seg] for i in range(0, seg*N, seg)]))
+s = sorted([l[i:i+seg] for i in range(0, seg*N, seg)])
+sys.stdout.buffer.write(o)
 
 for i in s:
-	o += i
-sys.stdout.buffer.write(o)
+	sys.stdout.buffer.write(i)
 
 
 
