d09959050471841546dba40a439580fb84bfcc35
Uberariy <70648019+Uberariy@users.noreply.github.com>
2021-12-10 22:51:59 +0300

    Task Correction

--- 20211014/3/task.py
+++ 20211014/3/task.py
@@ -1,5 +1,5 @@
+import math as math
 def lvlwhat(sttr, c):
-	#print(sttr[1:len(sttr)-1], c)
 	return c in sttr[1:len(sttr)-1]
 
 sttr = str(input()).split()[0];	sttr = str(input()).split()[0]
@@ -12,13 +12,14 @@ water = 1 if lvlwhat(sttr, "~") else 0
 while not lvlwhat(sttr, "#"):
 	sttr = str(input()).split()[0]
 	water += 1 if lvlwhat(sttr, "~") and not lvlwhat(sttr, "#") else 0
-#print(gas, water)
 L = water+gas
+
 print("#"*(L+2))
-for i in range(round(gas*H/L)):
+for i in range(math.trunc(gas*H/L)):
 	print("#"+"."*L+"#")
-for i in range(round(water*H/L)):
+for i in range(math.trunc(water*H/L)+1):
 	print("#"+"~"*L+"#")
 print("#"*(L+2))
-print("{:<"f"{max(gas*H, water*H)}""} {:>"f"{1+len(str(max(gas*H, water*H)))+len(str((water+gas)*H))}""}".format("."*gas*H,str(gas*H)+"/"+str((water+gas)*H)))
-print("{:<"f"{max(gas*H, water*H)}""} {:>"f"{1+len(str(max(gas*H, water*H)))+len(str((water+gas)*H))}""}".format("~"*water*H,str(water*H)+"/"+str((water+gas)*H)))
+o = int(max(round(gas/max(gas,water)*20), water/max(gas,water)*20))
+print("{:<"f"{o}""} {:>"f"{1+len(str(max(gas*H, water*H)))+len(str((water+gas)*H))}""}".format("."*round(gas/max(gas,water)*20),str(gas*H)+"/"+str((water+gas)*H)))
+print("{:<"f"{o}""} {:>"f"{1+len(str(max(gas*H, water*H)))+len(str((water+gas)*H))}""}".format("~"*round(water/max(gas,water)*20),str(water*H)+"/"+str((water+gas)*H)))
