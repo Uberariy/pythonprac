fbcd7c01258486e4e4e2a699c7977cf9f8cb257f
Uberariy <70648019+Uberariy@users.noreply.github.com>
2021-12-11 00:18:16 +0300

    Final Correction for today

--- 20211014/3/task.py
+++ 20211014/3/task.py
@@ -2,24 +2,27 @@ import math as math
 def lvlwhat(sttr, c):
 	return c in sttr[1:len(sttr)-1]
 
+def count(sttr, c):
+	cou = 0
+	for i in sttr[1:len(sttr)-1]:
+		cou = cou + 1 if i == c else cou
+	return cou
+
 sttr = str(input()).split()[0];	sttr = str(input()).split()[0]
 H = len(sttr)-2
-gas = 1 if lvlwhat(sttr, ".") and not lvlwhat(sttr, "~") and not lvlwhat(sttr, "#") else 0
-while not lvlwhat(sttr, "~") and not lvlwhat(sttr, "#"):
-	sttr = str(input()).split()[0]
-	gas += 1 if lvlwhat(sttr, ".") and not lvlwhat(sttr, "~") and not lvlwhat(sttr, "#") else 0
-water = 1 if lvlwhat(sttr, "~") else 0
+L = gas = water = 0
 while not lvlwhat(sttr, "#"):
+	gas += count(sttr, ".")
+	water += count(sttr, "~")
+	L += 1
 	sttr = str(input()).split()[0]
-	water += 1 if lvlwhat(sttr, "~") and not lvlwhat(sttr, "#") else 0
-L = water+gas
 
 print("#"*(L+2))
-for i in range(math.trunc(gas*H/L)):
+for i in range(math.trunc(gas/L)):
 	print("#"+"."*L+"#")
-for i in range(math.trunc(water*H/L)+1):
+for i in range(math.ceil(water/L)):
 	print("#"+"~"*L+"#")
 print("#"*(L+2))
 o = int(max(round(gas/max(gas,water)*20), water/max(gas,water)*20))
-print("{:<"f"{o}""} {:>"f"{1+len(str(max(gas*H, water*H)))+len(str((water+gas)*H))}""}".format("."*round(gas/max(gas,water)*20),str(gas*H)+"/"+str((water+gas)*H)))
-print("{:<"f"{o}""} {:>"f"{1+len(str(max(gas*H, water*H)))+len(str((water+gas)*H))}""}".format("~"*round(water/max(gas,water)*20),str(water*H)+"/"+str((water+gas)*H)))
+print("{:<"f"{o}""} {:>"f"{1+len(str(max(gas, water)))+len(str((water+gas)))}""}".format("."*round(gas/max(gas,water)*20),str(gas)+"/"+str((water+gas))))
+print("{:<"f"{o}""} {:>"f"{1+len(str(max(gas, water)))+len(str((water+gas)))}""}".format("~"*round(water/max(gas,water)*20),str(water)+"/"+str((water+gas))))
