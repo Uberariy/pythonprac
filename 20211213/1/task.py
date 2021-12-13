bfdf32e626093c0a6d68ce857d102c0d2d366c58
Uberariy <70648019+Uberariy@users.noreply.github.com>
2021-12-13 20:02:01 +0300

    First task done

--- 20211213/1/task.py
+++ 20211213/1/task.py
@@ -2,9 +2,14 @@ import random
 import asyncio
 import collections
 
-async def merge(b0, b1, e1, n):
-    b, e0, i = b0, b1, b0
-    while b0 < e0 and b1 < e1:
+async def merge(b0, b1, c1, eL, eR, eCurr):
+    if b1-b0 == 0:
+        await asyncio.sleep(0)
+    else:
+        await eL.wait()
+        await eR.wait()
+    b, c0, i = b0, b1, b0
+    while b0 < c0 and b1 < c1:
         if L[b0] < L[b1]:
             LL[i] = L[b0]
             b0 += 1
@@ -12,22 +17,21 @@ async def merge(b0, b1, e1, n):
             LL[i] = L[b1]
             b1 += 1
         i += 1
-        
-    await asyncio.sleep(0)
-    #print(f"-> {n}\t", end="")
-    LL[i:e1] = L[b0:e0] + L[b1:e1]
-    L[b:e1] = LL[b:e1]
+    LL[i:c1] = L[b0:c0] + L[b1:c1]
+    L[b:c1] = LL[b:c1]
+    eCurr.set()
 
 async def joiner():
-    n = 0
+    n = 0; tasks = []
+    E = collections.defaultdict(asyncio.Event)
     for p in range(4):
-        tasks = []
         b = 2**(p+1)
         for i in range(0, len(L), b):
-            E = collections.defaultdict(asyncio.Event)
-            tasks.append(asyncio.create_task(merge(i, i+b//2, i+b, n, E)))
+            tasks.append(asyncio.create_task(merge(i, i+b//2, i+b, E[f"{p}_{i}"], E[f"{p}_{i+b}"], E[f"{p}_{i+b//2}"])))
             n += 1
-        await asyncio.gather(*tasks)
+    for i in E:
+        E[i].set()
+    await asyncio.gather(*tasks)
 
 L = list(eval(input()))
 LL = L.copy()
