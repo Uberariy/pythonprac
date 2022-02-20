32c0a7ba25fc876dbb28910d197f7f7ed4016ace
Uberariy <70648019+Uberariy@users.noreply.github.com>
2022-02-20 20:25:52 +0300

    (5/5) Commit for 20220214
    5 commit from pipeline http://uneex.ru/LecturesCMC/PythonDevelopment2022/Prac/01_Intro

--- 20220214/1/task.py
+++ 20220214/1/task.py
@@ -13,16 +13,10 @@ elif len(sys.argv) == 2:
     with open(branchpath) as objfile:
         filecont = objfile.read()
     commitpath = pref+"/objects/"+filecont[:2]+"/"+filecont[2:-1]
-
-    with open(commitpath, "rb") as objfile:
-        fullobj = zlib.decompress(objfile.read())
-        header, _, body = fullobj.partition(b'\x00')
-        commitcont = body.decode().split()
-    treepath = pref+"/objects/"+commitcont[commitcont.index("tree")+1][:2]+"/"+commitcont[commitcont.index("tree")+1][2:]
     
-    def shag(treepath, depth):
+    def shag(treepath, depth, commitcont):
         if depth == 1:
-            print("|-=-=-[TREE: "+commitcont[commitcont.index("tree")+1]+"]-=-=-|")
+            print("\n|-=-=-[TREE: "+commitcont[commitcont.index("tree")+1]+"]-=-=-|")
         with open(treepath, "rb") as objfile:
             fullobj = zlib.decompress(objfile.read())
             header, _, body = fullobj.partition(b'\x00')
@@ -32,9 +26,25 @@ elif len(sys.argv) == 2:
                 gitid, body = tail[:20], tail[20:]
                 if treehdr.split()[0] == b'40000':
                     print("="*(depth+2)+"#\ "+f"{str(treehdr.split()[1])[2:-1]}, {gitid.hex()}")
-                    shag(pref+"/objects/"+gitid.hex()[:2]+"/"+gitid.hex()[2:], depth+1)
+                    shag(pref+"/objects/"+gitid.hex()[:2]+"/"+gitid.hex()[2:], depth+1, commitcont)
                 else:
                     print("-"*(depth-1)+"| "+f"{str(treehdr.split()[1])[2:-1]}, {gitid.hex()}")
             print("="*(depth+1)+"#/")
 
-    shag(treepath, 1)
+    def diveincommit(commitpath):
+        try:
+            with open(commitpath, "rb") as objfile:
+                fullobj = zlib.decompress(objfile.read())
+                header, _, body = fullobj.partition(b'\x00')
+                commitcont = body.decode().split()
+            treepath = pref+"/objects/"+commitcont[commitcont.index("tree")+1][:2]+"/"+commitcont[commitcont.index("tree")+1][2:]
+            shag(treepath, 1, commitcont)
+            if "parent" in commitcont:
+                parentcommit = pref+"/objects/"+commitcont[commitcont.index("parent")+1][:2]+"/"+commitcont[commitcont.index("parent")+1][2:]
+                diveincommit(parentcommit)
+            else:
+                raise FileNotFoundError
+        except FileNotFoundError as e:
+            print("\nCommit History Ends")
+    
+    diveincommit(commitpath)
