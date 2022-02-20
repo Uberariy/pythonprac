efb24f520496467be366f755088286955b54d75e
Uberariy <70648019+Uberariy@users.noreply.github.com>
2022-02-20 19:57:39 +0300

    (4/5) Commit for 20220214
    4 commit from pipeline http://uneex.ru/LecturesCMC/PythonDevelopment2022/Prac/01_Intro

--- 20220214/1/task.py
+++ 20220214/1/task.py
@@ -3,7 +3,7 @@ import sys
 from glob import iglob
 from os.path import join
 
-pref = ".git"   # Maybe "../../.git"
+pref = "../../.git"
 if len(sys.argv) == 1:
     branchpath = pref+"/refs/heads/*"
     for objname in iglob(branchpath):
@@ -20,10 +20,21 @@ elif len(sys.argv) == 2:
         commitcont = body.decode().split()
     treepath = pref+"/objects/"+commitcont[commitcont.index("tree")+1][:2]+"/"+commitcont[commitcont.index("tree")+1][2:]
     
-    with open(treepath, "rb") as objfile:
-        fullobj = zlib.decompress(objfile.read())
-        header, _, body = fullobj.partition(b'\x00')
-        while body:
-            treehdr, _, tail = body.partition(b'\x00')
-            gitid, body = tail[:20], tail[20:]
-            print(f"{treehdr}, {gitid.hex()}")
+    def shag(treepath, depth):
+        if depth == 1:
+            print("|-=-=-[TREE: "+commitcont[commitcont.index("tree")+1]+"]-=-=-|")
+        with open(treepath, "rb") as objfile:
+            fullobj = zlib.decompress(objfile.read())
+            header, _, body = fullobj.partition(b'\x00')
+
+            while body:
+                treehdr, _, tail = body.partition(b'\x00')
+                gitid, body = tail[:20], tail[20:]
+                if treehdr.split()[0] == b'40000':
+                    print("="*(depth+2)+"#\ "+f"{str(treehdr.split()[1])[2:-1]}, {gitid.hex()}")
+                    shag(pref+"/objects/"+gitid.hex()[:2]+"/"+gitid.hex()[2:], depth+1)
+                else:
+                    print("-"*(depth-1)+"| "+f"{str(treehdr.split()[1])[2:-1]}, {gitid.hex()}")
+            print("="*(depth+1)+"#/")
+
+    shag(treepath, 1)
