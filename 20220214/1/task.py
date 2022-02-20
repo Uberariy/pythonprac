ac5f1a7c276e807dfce4e745312ee8a19a813ceb
Uberariy <70648019+Uberariy@users.noreply.github.com>
2022-02-20 19:16:33 +0300

    3/5 Commit for 20220214
    3 commit from pipeline http://uneex.ru/LecturesCMC/PythonDevelopment2022/Prac/01_Intro

--- 20220214/1/task.py
+++ 20220214/1/task.py
@@ -13,8 +13,17 @@ elif len(sys.argv) == 2:
     with open(branchpath) as objfile:
         filecont = objfile.read()
     commitpath = pref+"/objects/"+filecont[:2]+"/"+filecont[2:-1]
+
     with open(commitpath, "rb") as objfile:
         fullobj = zlib.decompress(objfile.read())
         header, _, body = fullobj.partition(b'\x00')
-        kind, size = header.split()
-        print(body.decode())
+        commitcont = body.decode().split()
+    treepath = pref+"/objects/"+commitcont[commitcont.index("tree")+1][:2]+"/"+commitcont[commitcont.index("tree")+1][2:]
+    
+    with open(treepath, "rb") as objfile:
+        fullobj = zlib.decompress(objfile.read())
+        header, _, body = fullobj.partition(b'\x00')
+        while body:
+            treehdr, _, tail = body.partition(b'\x00')
+            gitid, body = tail[:20], tail[20:]
+            print(f"{treehdr}, {gitid.hex()}")
