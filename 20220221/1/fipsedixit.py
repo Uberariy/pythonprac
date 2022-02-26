1c7162dfede01acf2afb572683e20264e828281b
Uberariy <70648019+Uberariy@users.noreply.github.com>
2022-02-26 13:57:15 +0300

    Task done

---
+++ 20220221/1/fipsedixit.py
@@ -0,0 +1,16 @@
+from ipsedixit import Generator, parse_args
+import sys
+
+if (len(sys.argv) > 2) and (sys.argv[2][0] != '-'):
+    bpath = sys.argv[2]
+    sys.argv.pop(2)
+    if (bpath not in ('caesar', 'tacitus')):
+        with open(bpath, 'r') as f:
+            text = f.read()
+    else:
+        text = bpath
+    generator = Generator(text)
+else:
+    generator = Generator()
+args = parse_args()
+print('\n\n'.join(generator.paragraphs(args.num, args.min, args.max)))
