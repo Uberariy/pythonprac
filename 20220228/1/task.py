2faf974bd695f2a995e3805feb6d3b61337e5171
Uberariy <uberariy@WIN-E9EQ5986VJ3.localdomain>
2022-03-03 12:09:16 +0300

    2: 3/3

--- 20220228/1/task.py
+++ 20220228/1/task.py
@@ -1,7 +1,12 @@
 import textdistance
 
 def dist(s1, s2, m):
-    return textdistance.levenshtein(s1, s2)
+    if m == "D":
+        return textdistance.damerau_levenshtein(s1, s2)
+    elif m == "L":
+        return textdistance.levenshtein(s1, s2)
+    else:
+        return -1
 
 s1, s2, m = str(input()), str(input()), str(input())
 res = dist(s1, s2, m)
