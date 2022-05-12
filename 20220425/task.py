0e5765d707fc05f42d01538e3333108a4bbf1bbb
Uberariy <a3.okonishnikov@mail.ru>
2022-05-13 02:15:18 +0300

    Prac finished

--- 20220425/task.py
+++ 20220425/task.py
@@ -2,7 +2,7 @@ import sys
 import pyfiglet
 import gettext
 
-translation = gettext.translation('messages', 'po', fallback=True)
+translation = gettext.translation('task', 'po', fallback=True)
 _, ngettext = translation.gettext, translation.ngettext
 
 
@@ -16,7 +16,9 @@ if __name__ == "__main__":
     if len(sys.argv) == 3:
         res = solve(float(sys.argv[1]), float(sys.argv[2]))
         if res is None:
-            print(pyfiglet.figlet_format(_("NO ROOTS")))
+            print(pyfiglet.figlet_format(_("NO ROOTS"), font='graceful'))
         else:
-            print(pyfiglet.figlet_format(_("Root: {}").format(res)))
+            print(pyfiglet.figlet_format(_("Root: {}").format(res), font='graceful'))
+    else:
+        sys.exit("Wrong number of arguments.")
 
